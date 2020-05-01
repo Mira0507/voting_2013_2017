950b0dc423f20ace3124f9a92e8e6aac17b58ed6
Mira Sohn <tonton07@gmail.com>
2020-05-01 00:56:49 -0400

    code

--- code1.R
+++ code1.R
@@ -184,3 +184,5 @@ relative_electoral_votes <- data.frame(Top_States = nss9$State[1:10],
 relative_electoral_votes_table <- formattable(relative_electoral_votes,
                                               list(Top_Votes = color_tile("white", "paleturquoise"),
                                                    Bottom_Votes = color_tile("white", "mediumaquamarine")))
+rel_elec_col_vs_voter_cor <- cor(nss9$Adult_Citizen_Population,
+                                 nss9$Electoral_College_Per_Adult_Citizen_Population)
